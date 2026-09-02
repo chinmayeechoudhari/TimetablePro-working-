ff7351faa2dec5fb1e60d9b92ece3c4f3518d9a9
CHINMAYEE AVINASH CHOUDHARI <chinmayeechoudhari92@gmail.com>
2026-09-02 16:44:52 +0530

    fix: apply final academic structure spacing

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import './theme.css'
 import './components/AcademicStructureLayoutFix.css'
+import './components/AcademicStructureFinalLayout.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
