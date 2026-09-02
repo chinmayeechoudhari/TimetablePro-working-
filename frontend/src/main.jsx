8fa7121e131288c68642c3f90a1c8d68d93892e1
CHINMAYEE AVINASH CHOUDHARI <chinmayeechoudhari92@gmail.com>
2026-09-02 16:44:19 +0530

    style: load academic structure layout refinements

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import './theme.css'
+import './components/AcademicStructureLayoutFix.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
